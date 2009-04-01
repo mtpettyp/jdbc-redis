d5bf60b11fff95b5bacf3777812fc5c15872c490
mavcunha <mavcunha@2e1e241e-19cd-11de-b69a-45dc1c5e4070>
2009-04-01 23:22:57 +0000

    Removing warning by providing a serialVersion to RedisResultException

--- trunk/src/br/com/svvs/jdbc/redis/RedisResultException.java
+++ trunk/src/br/com/svvs/jdbc/redis/RedisResultException.java
@@ -2,6 +2,8 @@ package br.com.svvs.jdbc.redis;
 
 public class RedisResultException extends Exception {
 
+	private static final long serialVersionUID = 1L;
+	
 	public RedisResultException() {}
 	public RedisResultException(String msg) {
 		super(msg);
